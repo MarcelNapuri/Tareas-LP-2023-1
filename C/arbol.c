a2d452fe2e65f8eafde4bbeef32a144c96c84856
Marcel Silva <marcelsilvanapuri@gmail.com>
2023-04-20 00:21:45 -0400

    agregadas funciones ls y ls_dir

--- C/arbol.c
+++ C/arbol.c
@@ -1,5 +1,6 @@
 #include "arbol.h"
 #include <stdlib.h>
+#include <iostream>
 
 Lista* crear_lista(int largo_maximo_inicial) {
     Lista* lista = (Lista*)malloc(sizeof(Lista)); // Asigna memoria para la estructura lista
@@ -12,7 +13,7 @@ Lista* crear_lista(int largo_maximo_inicial) {
 void insertar_lista(Lista* lista, Nodo* nodo) {
     if (lista->largo_actual >= lista->largo_maximo) { // Si el largo actual es mayor o igual al máximo, entonces duplica el tamaño del arreglo
         lista->largo_maximo *= 2;
-        lista->arreglo = realloc(lista->arreglo,sizeof(Nodo) * lista->largo_maximo);
+        lista->arreglo = (Nodo*)realloc(lista->arreglo,sizeof(Nodo) * lista->largo_maximo);
     }
     lista->arreglo[lista->largo_actual] = *nodo; // Agrega el nodo al final del arreglo y aumenta el largo actual
     lista->largo_actual++;
@@ -47,11 +48,8 @@ Nodo* crear_nodo(Nodo* padre, char* tipo, char* nombre) {    //son nodos vacios
     nodo->padre = padre;
     nodo->tipo = tipo;
     nodo->contenido = NULL;
-
-    return nodo; 
-    
+    return nodo;     
 }
-
 
 void mkdir(Nodo* actual, char * nombre_directorio){
     Nodo* nuevo_nodo = crear_nodo(actual, "Directorio" , nombre_directorio);
@@ -71,4 +69,28 @@ void touch(Nodo* actual, char* nombre_archivo){
     insertar_lista(((Directorio*)actual->contenido)->hijos, nuevo_nodo);
 }
 
+void write(Nodo* actual, char* nombre_archivo , char* contenido){
+    Nodo* nodo_archivo = buscar_archivo(((Directorio*)actual->contenido)->hijos,nombre_archivo);
+    ((Archivo*)nodo_archivo->contenido)->contenido = contenido;
+}
 
+void car(Nodo* actual, char* nombre_archivo){
+    Nodo* nodo_archivo = buscar_archivo(((Directorio*)actual->contenido)->hijos,nombre_archivo);
+    char* texto = ((Archivo*)nodo_archivo->contenido)->contenido;
+    printf("%s" , texto);
+}
+
+void ls(Nodo* actual){
+    for(int i = 0; i < ((Directorio*)actual->contenido)->hijos->largo_actual; i++){
+        Nodo* hijo = ((Directorio*)actual->contenido)->hijos->arreglo[i];
+        printf("%s\n", ((Directorio*)hijo->contenido)->nombre);
+    }
+}
+
+void ls_dir(Nodo* actual, char* nombre_directorio){
+    Nodo* nuevo_directorio = buscar_directorio(((Directorio*)actual->contenido)->hijos,nombre_directorio);
+    for(int i = 0; i < ((Directorio*)nuevo_directorio->contenido)->hijos->largo_actual; i++){
+        Nodo* hijo = ((Directorio*)nuevo_directorio->contenido)->hijos->arreglo[i];
+        printf("%s\n", ((Directorio*)hijo->contenido)->nombre);
+    }
+}
